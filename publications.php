9e74b626d53102cc6f7456983adcf5e7ca62a8ca
Jacob Bender <jakebender@Jakes-MacBook-Pro-5.local>
2018-02-14 19:20:57 -0500

    FirstPush_Skeleton_Homepage

---
+++ publications.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>HOME</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css?family=Oswald|Raleway:500" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Oswald|Raleway:500" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <link rel="stylesheet" type="text/css" href="normalize.css" />
+</head>
+<body>
+<div id="wrapper">
+    <div id="main">
+
+    <div class="head_menu">
+    <a id="home" href="index.php"><img src="http://via.placeholder.com/50x50"></a>
+    <a href="about.php">About</a>
+    <a href="publications.php">Publications</a>
+    <a href="contact.php">Contact</a>
+   </div>
+
+    <div id="missionBox">
+    <h1>Games and Learning In Digital Environments</h1>
+    </div>
+
+    <div id="theDiv">
+    <p>"this would be an overview of our message at glide, and the very first thing that people would read. It has to
+    be about what this company represents, and what we do at glide that makes us unique and special. Maybe a quote from
+    Doctor Foster's papers mayhaps, or something here that we could stylize it and make an image of it in this box.</p>
+    </div>
+
+    <div class="visual_menu">
+    <a id="current" href="current.php"><img src="http://via.placeholder.com/250x150"></a>
+    <a id="past" href="past.php"><img src="http://via.placeholder.com/250x150"></a>
+    <a id="team" href="team.php"><img src="http://via.placeholder.com/250x150"></a>
+    <a id="withUs" href="withUs.php"><img src="http://via.placeholder.com/250x150"></a>
+    </div>
+
+    </div>  
+
+    <div id="footer">
+        <p>Glide Labs 2018</p>
+    </div>      
+</div> 
+<script src="main.js"></script>
+</body>
+</html>
